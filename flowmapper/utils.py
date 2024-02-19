59004767a5e2fb72871b6e2ae9245c3469aa094f
Chris Mutel <cmutel@gmail.com>
2024-02-19 08:04:58 +0100

    Fix country code extraction to only use existing countries

--- flowmapper/utils.py
+++ flowmapper/utils.py
@@ -91,9 +91,30 @@ def rm_roman_numerals_ionic_state(s: str):
     pattern = r'\s*\(\s*[ivxlcdm]+\s*\)'
     return re.sub(pattern, '', s)
 
+
+COUNTRY_CODES = "|".join({'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AN', 'AO', 'AQ', 'AR', 'AS', 'AT',
+    'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BM', 'BN', 'BO',
+    'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ', 'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL',
+    'CM', 'CN', 'CO', 'CR', 'CS', 'CU', 'CV', 'CX', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ',
+    'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET', 'FI', 'FJ', 'FK', 'FM', 'FO', 'FR', 'GA', 'GB', 'GD',
+    'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY',
+    'HK', 'HM', 'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS', 'IT',
+    'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW', 'KY', 'KZ', 'LA',
+    'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY', 'MA', 'MC', 'MD', 'MG', 'MH', 'MK',
+    'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA',
+    'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ', 'OM', 'PA', 'PE', 'PF', 'PG',
+    'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY', 'QA', 'RE', 'RO', 'RU', 'RW', 'SA',
+    'SB', 'SC', 'SD', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'ST', 'SV',
+    'SY', 'SZ', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV',
+    'TW', 'TZ', 'UA', 'UG', 'UM', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU', 'WF',
+    'WS', 'YE', 'YT', 'ZA', 'ZM', 'ZW'})
+
+# Regex to find a two-letter uppercase code following a comma and optional whitespace
+country_code_regex = re.compile(r',\s*({})$'.format(COUNTRY_CODES))
+
+
 def extract_country_code(s: str) -> tuple[str, Optional[str]]:
-    # Regex to find a two-letter uppercase code following a comma and optional whitespace
-    match = re.search(r',\s*([a-z]{2})$', s)
+    match = re.search(country_code_regex, s)
 
     if match:
         # Extract the country code and the preceding part of the string
--- tests/test_extract_country_code.py
+++ tests/test_extract_country_code.py
@@ -1,13 +1,16 @@
 from flowmapper.utils import extract_country_code
 
+def test_with_lowercase():
+    assert extract_country_code('ammonia, nl') == ('ammonia, nl', None)
+
 def test_with_valid_country_code():
-    assert extract_country_code('ammonia, nl') == ('ammonia', 'nl')
+    assert extract_country_code('ammonia, NL') == ('ammonia', 'NL')
 
 def test_with_no_country_code():
     assert extract_country_code('ammonia') == ('ammonia', None)
 
 def test_with_additional_text():
-    assert extract_country_code('ammonia, nl something') == ('ammonia, nl something', None)
+    assert extract_country_code('ammonia, NL something') == ('ammonia, NL something', None)
 
 def test_with_space_before_country_code():
-    assert extract_country_code('ammonia,   nl') == ('ammonia', 'nl')
+    assert extract_country_code('ammonia,   NL') == ('ammonia', 'NL')

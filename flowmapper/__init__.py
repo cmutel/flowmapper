63160a335573d5bd6eddef84dc374b9d7eb8dbe7
Chris Mutel <cmutel@gmail.com>
2024-04-19 15:34:58 +0200

    Add CAS to main library imports

--- flowmapper/__init__.py
+++ flowmapper/__init__.py
@@ -1,11 +1,11 @@
-"""flowmapper."""
-
-from .flowmap import Flowmap
-
 __all__ = (
     "__version__",
     "Flowmap",
+    "CAS",
     # Add functions and variables you want exposed in `flowmapper.` namespace here
 )
 
 __version__ = "0.0.0-9010"
+
+from .flowmap import Flowmap
+from .cas import CAS

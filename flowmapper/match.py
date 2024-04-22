1e138aacd2d8bce4ea4d3274c3683483bf85dff4
Chris Mutel <cmutel@gmail.com>
2024-04-22 14:25:46 +0200

    Special case regionalized water unit conversion

--- flowmapper/match.py
+++ flowmapper/match.py
@@ -32,9 +32,10 @@ def match_identical_identifier(s: Flow, t: Flow, comment: str = "Identical ident
 def match_identical_names_in_synonyms(
     s: Flow, t: Flow, comment: str = "Identical synonyms"
 ):
-    if (t.synonyms and s.name in t.synonyms and s.context == t.context) or (
-        s.synonyms and t.name in s.synonyms and s.context == t.context
-    ):
+    if (
+        (t.synonyms and s.name in t.synonyms and s.context == t.context)
+        or (s.synonyms and t.name in s.synonyms and s.context == t.context)
+    ) and s.unit.conversion_factor(t.unit):
         return {"comment": comment}
 
 
@@ -85,7 +86,22 @@ def match_names_with_roman_numerals_in_parentheses(
 def match_names_with_country_codes(s: Flow, t: Flow, comment="Names with country code"):
     s_name, s_location = extract_country_code(s.name.normalized)
     if s_location and s_name == t.name and s.context == t.context:
-        return {"comment": comment, "location": s_location.upper()}
+        result = {"comment": comment, "location": s_location.upper()}
+        if (
+            s.name.normalized.lower().startswith("water")
+            and t.name.normalized.lower().startswith("water")
+            and s.unit.normalized == "cubic_meter"
+            and t.unit.normalized == "kilogram"
+        ):
+            result["conversion_factor"] = 1000
+        elif (
+            s.name.normalized.lower().startswith("water")
+            and t.name.normalized.lower().startswith("water")
+            and t.unit.normalized == "cubic_meter"
+            and s.unit.normalized == "kilogram"
+        ):
+            result["conversion_factor"] = 0.001
+        return result
 
 
 def match_non_ionic_state(
